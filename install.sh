abe2cfa8d4d5e22ca17fe1cdf19f214b1f23d279
Shaon Diwakar <shaon@shaon.net>
2025-11-21 16:35:23 +1100

    build: fix the build script so that we aren't copying the binary
    Claude made some errors where it was copying the binary into the Alfred
    folder 🤷♂️ not sure why? 🤔

--- install.sh
+++ install.sh
@@ -11,11 +11,11 @@ echo "1. Compiling Rust binary..."
 cargo build --release
 
 # Copy binary to workflow directory
-echo "2. Copying binary to workflow directory..."
-cp target/release/almoji Almoji.alfredworkflow/
+# echo "2. Copying binary to workflow directory..."
+# cp target/release/almoji Almoji.alfredworkflow/
 
 # Create the Alfred workflow package (zip file)
-echo "3. Creating Alfred workflow package..."
+echo "2. Creating Alfred workflow package..."
 cd Almoji.alfredworkflow
 zip -r ../Almoji-Workflow.alfredworkflow * -x "*.DS_Store"
 cd ..
